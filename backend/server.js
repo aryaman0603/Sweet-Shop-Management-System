390a15473b2fabd3d1bbf11dc5af6e4ac420f4c4
aryaman0603 <aryamansharma3319@gmail.com>
2025-09-23 00:41:53 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ dotenv.config();
 connectDb();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -31,5 +33,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
